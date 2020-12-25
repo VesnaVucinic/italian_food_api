125ca1101c409fba4472d29db5f1afc234abb6c4
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-25 22:12:23 +0000

    add scope methodes for submitted and finished, and #total

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,13 @@ class Order < ApplicationRecord
     has_many :dishes, through: :order_dishes
 
     validates_inclusion_of :status, :in => ['submitted', 'finished']
+    validates :date, :user_id, presence:true
 
+    scope :submitted,-> { where(status: 'submitted') }
+    scope :finished, -> { where(status: 'finished') }
+
+    def total
+        self.dishes.reduce(0){|sum,dish|sum+dish.price}
+    end
     
 end
