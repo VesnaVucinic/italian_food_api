2e006fed72bd0cc8d947ed6c56eced8c9293d52a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-25 21:43:35 +0000

    add show action and user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,17 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def show
+        user = User.find_by id:params[:id]
+        if user
+           render json: user, include: ['orders','orders.dishes']
+        else 
+           render json: {error: 'Error findind account'}
+        end
+    end
+
+    def user_params
+        params.permit(:first_name, :last_name, :password, :email)
+      end
 end
