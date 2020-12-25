c29708a559313c9b28eadc963bd85ed89c4e05e8
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-25 20:57:49 +0000

    add routes for Order and Dish models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create, :show]
+      resources :users, only: [:index, :create, :show] do
+        resources :orders
+      end
+      resources :dishes
     end
   end 
 end
