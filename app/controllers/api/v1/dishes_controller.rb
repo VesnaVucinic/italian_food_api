6f7ae7bc9f2ec18f47b5f0d0d42283e4b2c46655
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-25 21:56:09 +0000

    add index and show action

--- app/controllers/api/v1/dishes_controller.rb
+++ app/controllers/api/v1/dishes_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::DishesController < ApplicationController
+    def index
+        dishes = Dish.all
+        render json: dishes
+    end
+
+
+private
+
+   def dish_params
+    params.require(:dish).permit(:name, :image_url, :description)
+   end
 end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::OrdersController < ApplicationController
+
+    def index 
+        orders = @user.orders
+        render json: orders
+    end
+
+    def show
+        order = Order.find(params[:id])
+        render json: order
+    end
+
+
 end
